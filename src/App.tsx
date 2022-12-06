8c46d363b3ea585ccec171a7a0dbe14d72c79ca1
Pablo <pablosalgadou@gmail.com>
2022-12-06 15:39:46 -0300

    new layout with vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,34 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import proinLogo from "./images/logo-png.png";
-import "./App.css";
-import myFirebase, { formCollection } from "./Firebase/fbConfig";
-import Spinner from "./images/Walk.gif";
-import Formularios from "./FormularioPrincipal";
-import DivDeTrabajo from "./VisorDeTrabajos/components/DivDeTrabajo";
-import VisorDeFormulario from "./FormularioPrincipal/components/VisorDeFormulario";
-import DivDeFreelancer from "./VisorDeTrabajos/components/DivDeFreelancer";
-import Navbar from "./utils/Navbar";
-import VisorDeItems from "./utils/VisorDeItems";
-import DivDeSocios from "./VisorDeTrabajos/components/DivDeSocios";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-//Rralizar cambio de estado segun oferta que se entregue al argumento y con eso estamos listos
 function App() {
-  const [tituloBusqueda, setTituloBusqueda] = useState<String>("Trabajos");
-  const [trabajos, setTrabajos] = useState<any>([]);
-  const activeDiv = useRef(null);
-  const getTitulo = (title) => {
-    setTituloBusqueda(title);
-  };
-  const getData = async (collectionName) => {
-    setTrabajos([]);
-    const data: Object[] = [];
-    const firstData = await myFirebase.firestore
-      .collection(collectionName).orderBy('creadoEn',"desc")
-      .get();
-
-    if (firstData.empty) {
-      console.log("No documentos");
-      return;
-    }
-    firstData.forEach((doc) => {
-      data.push(doc.data());
-    });
-    setTrabajos([...data]);
-  };
-
-  const getJobs = () => {
-    getData(formCollection.ofertaLaboral);
-    getData(formCollection.ofertaLaboralFreelance);
-    getData(formCollection.contizacionProyecto);
-  };
-  const getPartners = () => {
-    getData(formCollection.busquedaDeSocio);
-  };
-  const getFreelancers = () => {
-    getData(formCollection.viernesDePega);
-  };
-  useEffect(() => {
-    getJobs();
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App h-auto bg-gradient-to-t from-green-400 via-blue-500 to-blue-650 ">
-      <VisorDeFormulario />
-      <main className="font-body p-20  bg-white mt-9">
-        <h2 className="block text-gray-700 text-4xl font-bold my-2">
-          Ultimos {tituloBusqueda}
-        </h2>
-        <Navbar
-          getTitulo={getTitulo}
-          getJobs={getJobs}
-          getPartners={getPartners}
-          getFreelancers={getFreelancers}
-        ></Navbar>
-
-        {tituloBusqueda === "Trabajos" && (
-          <VisorDeItems
-            col={true}
-            data={trabajos}
-            render={(data) => <DivDeTrabajo t={data} />}
-          />
-        )}
-        {tituloBusqueda === "Socios" && (
-          <VisorDeItems
-            col={true}
-            data={trabajos}
-            render={(data) => <DivDeSocios socio={data}  />}
-          />
-        )}
-        {tituloBusqueda === "Freelancers" && (
-          <VisorDeItems
-            col={false}
-            data={trabajos}
-            render={(data) => <DivDeFreelancer perfil={data}/>}
-          />
-        )}
-      </main>
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/utils/fechaDesde.js
+++
@@ -1,8 +0,0 @@
-
-export function compareDates(d1) {
-  const date1 = new Date(d1);
-  const date2 = new Date();
-  var Difference_In_Time = date2.getTime() - date1.getTime();
-  var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-  return Math.floor(Difference_In_Days);
-}
